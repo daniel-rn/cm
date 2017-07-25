83fcad76272a5f6db2d1cab4629dd25316f3f0d9
danieldrn <daniel.nascimento@escolarmanager.com.br>
2017-07-25 08:18:59 -0300

    0006

---
+++ SISTEM.ALUNOS.CADASTRO/ClassLibrary1/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirebirdSql.Data.FirebirdClient;
+using System.Data;
+using System.Data.Common;
+
+namespace ClassLibrary1
+{
+    public static class Connection
+    {
+        //variáveis de conexão e acesso
+        static IDbConnection fbCnn;
+        static DbCommandBuilder fbCmm = new FbCommandBuilder();
+
+        public static Fb
+    }
+}
