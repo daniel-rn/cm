ff98790ebfa09169fa56d93188fef82dcb19094a
danieldrn <danielnascimento@escolarmanager.com>
2017-07-24 21:56:24 -0300

    0003 terceito commit

--- SISTEM.ALUNOS.CADASTRO/SISTEM.ALUNOS.CADASTRO/LoginForm.Designer.cs
+++ SISTEM.ALUNOS.CADASTRO/SISTEM.ALUNOS.CADASTRO/LoginForm.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.lbSenha = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(184, 163);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(77, 27);
+            this.btnEntrar.TabIndex = 0;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.Location = new System.Drawing.Point(30, 57);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lbUsuario.TabIndex = 1;
+            this.lbUsuario.Text = "Usuario";
+            // 
+            // lbSenha
+            // 
+            this.lbSenha.AutoSize = true;
+            this.lbSenha.Location = new System.Drawing.Point(33, 96);
+            this.lbSenha.Name = "lbSenha";
+            this.lbSenha.Size = new System.Drawing.Size(38, 13);
+            this.lbSenha.TabIndex = 2;
+            this.lbSenha.Text = "Senha";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(616, 363);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(301, 235);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbSenha);
+            this.Controls.Add(this.lbUsuario);
+            this.Controls.Add(this.btnEntrar);
+            this.Name = "LoginForm";
+            this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label lbUsuario;
+        private System.Windows.Forms.Label lbSenha;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
