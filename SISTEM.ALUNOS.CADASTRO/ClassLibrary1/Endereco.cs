ae05dc2dd642ea6ac61bc9fca5bfb8282ab419a2
danieldrn <daniel.nascimento@escolarmanager.com.br>
2017-10-09 09:43:18 -0300

    M0014 - Adiciona classes usuario, endereco, IEndereco

---
+++ SISTEM.ALUNOS.CADASTRO/ClassLibrary1/Endereco.cs
@@ -0,0 +1,11 @@
+﻿namespace CORE
+{
+    public class Endereco
+    {
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Estado { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+    }
+}
---
+++ SISTEM.ALUNOS.CADASTRO/ClassLibrary1/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace CORE
+{
+    public abstract class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
